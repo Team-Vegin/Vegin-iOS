dba3404880a81c1595f401dee91697eb6b74d257
eunjuchoi <eunjuzzoo@naver.com>
2021-11-10 13:11:36 +0900

    ➕[ADD] : 프로젝트 파일 생성

---
+++ Vegin_iOS/Vegin_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Vegin_iOS
+//
+//  Created by EUNJU on 2021/11/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
