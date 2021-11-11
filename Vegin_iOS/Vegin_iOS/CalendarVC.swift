07bd00cb344c1a4974c2624eb7e1d54db97c7b55
eunjuchoi <eunjuzzoo@naver.com>
2021-11-11 23:35:51 +0900

    ✨[FEAT] : 캘린더 헤더 UI 수정, 버튼 구현 (#1)

--- Vegin_iOS/Vegin_iOS/CalendarVC.swift
+++ Vegin_iOS/Vegin_iOS/CalendarVC.swift
@@ -11,11 +11,46 @@ import FSCalendar
 
 class CalendarVC: UIViewController {
 
+    @IBOutlet weak var headerLabel: UILabel!
     @IBOutlet weak var calendar: FSCalendar!
+    
+    private var currentPage: Date?
+    private lazy var today: Date = {
+        return Date()
+    }()
+    
+    private lazy var dateFormatter: DateFormatter = {
+        let df = DateFormatter()
+        df.locale = Locale(identifier: "ko_KR")
+        df.dateFormat = "yyyy년 M월"
+        return df
+        
+    }()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setCalendarUI()
+        setCalendarDelegate()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setCalendarUI()
+    }
+    
+    @IBAction func prevButtonTapped(_ sender: UIButton) {
+        scrollCurrentPage(isPrev: true)
+    }
+    @IBAction func nextButtonTapped(_ sender: Any) {
+        scrollCurrentPage(isPrev: false)
+    }
+    
+    private func scrollCurrentPage(isPrev: Bool) {
+        let cal = Calendar.current
+        var dateComponents = DateComponents()
+        dateComponents.month = isPrev ? -1 : 1
+        
+        self.currentPage = cal.date(byAdding: dateComponents, to: self.currentPage ?? self.today)
+        self.calendar.setCurrentPage(self.currentPage!, animated: true)
     }
 }
 
@@ -26,11 +61,19 @@ extension CalendarVC: FSCalendarDelegate, FSCalendarDataSource {
     }
     
     func setCalendarUI() {
+        calendar.calendarHeaderView.isHidden = true
+        calendar.headerHeight = 0
+        calendar.scope = .month
+        
         calendar.appearance.titleDefaultColor = .black
         calendar.appearance.headerTitleColor = .black
-        calendar.appearance.headerTitleFont = UIFont.boldSystemFont(ofSize: 16)
-        calendar.appearance.headerDateFormat = "YYYY년 M월"
         calendar.appearance.weekdayTextColor = .gray
         calendar.appearance.headerMinimumDissolvedAlpha = 0
+        
+        headerLabel.text = self.dateFormatter.string(from: calendar.currentPage)
+    }
+    
+    func calendarCurrentPageDidChange(_ calendar: FSCalendar) {
+        self.headerLabel.text = self.dateFormatter.string(from: calendar.currentPage)
     }
 }
