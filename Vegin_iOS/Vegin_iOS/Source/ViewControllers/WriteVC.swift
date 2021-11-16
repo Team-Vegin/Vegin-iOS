efc1b8b4fc4257049b6778630eaea1fd87c724e7
eunjuchoi <eunjuzzoo@naver.com>
2021-11-16 13:16:02 +0900

    ✨[FEAT]: 식단 기록 뷰 전체 UI 구현 (#3)

---
+++ Vegin_iOS/Vegin_iOS/Source/ViewControllers/WriteVC.swift
@@ -0,0 +1,25 @@
+//
+//  WriteVC.swift
+//  Vegin_iOS
+//
+//  Created by EUNJU on 2021/11/16.
+//
+
+import UIKit
+
+class WriteVC: UIViewController {
+
+    @IBOutlet weak var imageContentView: UIView!
+    @IBOutlet weak var buttonContentView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUI()
+    }
+    
+    private func setUI() {
+        imageContentView.layer.cornerRadius = 25
+        buttonContentView.layer.cornerRadius = 20
+    }
+
+}
