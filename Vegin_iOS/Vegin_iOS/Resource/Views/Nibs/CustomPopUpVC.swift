92afc9aa5239d8d4e75af366020840948803b5a9
eunjuchoi <eunjuzzoo@naver.com>
2021-11-24 07:18:06 +0900

    ✅[CHORE] : 커스텀 탭바 수정, 화면전환 구현 (#3)

--- Vegin_iOS/Vegin_iOS/Resource/Views/Nibs/CustomPopUpVC.swift
+++ Vegin_iOS/Vegin_iOS/Resource/Views/Nibs/CustomPopUpVC.swift
@@ -12,11 +12,13 @@ class CustomPopUpVC: UIViewController {
     @IBOutlet weak var alertView: UIView!
     @IBOutlet weak var iconImage: UIImageView!
     @IBOutlet weak var okButton: UIButton!
+    @IBOutlet weak var farmCheckButton: UIButton!
     @IBOutlet weak var titleLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         okButton.addTarget(self, action: #selector(okButtonClicked(_sender:)), for: .touchUpInside)
+        farmCheckButton.addTarget(self, action: #selector(farmCheckButtonClicked(_sender:)), for: .touchUpInside)
         setUI()
 
     }
@@ -24,15 +26,26 @@ class CustomPopUpVC: UIViewController {
     private func setUI() {
         alertView.layer.cornerRadius = 20
     }
+    
     @objc func okButtonClicked(_sender: UIButton) {
         //dismiss(animated: true, completion: nil)
         let tabBarStoryBoard = UIStoryboard.init(name: "Main", bundle: nil)
         guard let nextVC = tabBarStoryBoard.instantiateViewController(withIdentifier: TabBarController.className) as? TabBarController else { return }
         
+        nextVC.selectedIndex = 0
         nextVC.modalPresentationStyle = .fullScreen
         nextVC.modalTransitionStyle = .crossDissolve
         present(nextVC, animated: true, completion: nil)
-        
     }
-
+    
+    @objc func farmCheckButtonClicked(_sender: UIButton) {
+        //dismiss(animated: true, completion: nil)
+        let tabBarStoryBoard = UIStoryboard.init(name: "Main", bundle: nil)
+        guard let nextVC = tabBarStoryBoard.instantiateViewController(withIdentifier: TabBarController.className) as? TabBarController else { return }
+        
+        nextVC.selectedIndex = 1
+        nextVC.modalPresentationStyle = .fullScreen
+        nextVC.modalTransitionStyle = .crossDissolve
+        present(nextVC, animated: true, completion: nil)
+    }
 }
---
+++ Vegin_iOS/Vegin_iOS/Source/ViewControllers/FarmVC.swift
@@ -0,0 +1,17 @@
+//
+//  FarmVC.swift
+//  Vegin_iOS
+//
+//  Created by EUNJU on 2021/11/24.
+//
+
+import UIKit
+
+class FarmVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+}
