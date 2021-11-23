f65601139dad62958cc4dc2ee58975c936fd987d
eunjuchoi <eunjuzzoo@naver.com>
2021-11-23 13:01:06 +0900

    ✨[FEAT] : textView 클릭시 키보드 처리 기능 구현 (#3)

--- Vegin_iOS/Vegin_iOS/Source/ViewControllers/WriteVC.swift
+++ Vegin_iOS/Vegin_iOS/Source/ViewControllers/WriteVC.swift
@@ -11,11 +11,13 @@ class WriteVC: UIViewController {
 
     @IBOutlet weak var imageContentView: UIView!
     @IBOutlet weak var buttonContentView: UIView!
+    @IBOutlet weak var memoTextView: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setUI()
         hideKeyboardWhenTappedAround()
+        initUI()
     }
     
     @IBAction func touchUpToGoBack(_ sender: Any) {
@@ -33,6 +35,37 @@ class WriteVC: UIViewController {
     private func setUI() {
         imageContentView.layer.cornerRadius = 25
         buttonContentView.layer.cornerRadius = 20
+    }
+}
+
+extension WriteVC: UITextViewDelegate {
+    @objc
+    func keyboardWillShow(_ sender: Notification) {
+        if let keyboardFrame = sender.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+            let keyboardRectangle = keyboardFrame.cgRectValue
+            let keyboardHeight = keyboardRectangle.height
+            self.view.frame.origin.y = -keyboardHeight
+        }
+    }
+    
+    @objc
+    func keyboardWillHide(_ sender: Notification) {
+        self.view.frame.origin.y = 0
+    }
+}
+
+extension WriteVC {
+    func initUI() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillShow(_:)),
+                                               name: UIResponder.keyboardWillShowNotification,
+                                               object: nil)
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillHide(_:)),
+                                               name: UIResponder.keyboardWillHideNotification,
+                                               object: nil)
+        memoTextView.delegate = self
     }
 }
 
