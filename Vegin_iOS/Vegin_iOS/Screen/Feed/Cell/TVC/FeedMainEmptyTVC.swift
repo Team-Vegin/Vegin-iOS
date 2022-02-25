bbae5032be44ed5e85eaf2464398db4bf7c14f49
eunjuchoi <eunjuzzoo@naver.com>
2022-02-25 22:15:11 +0900

    ➕[ADD] : 피드 탭 메인 둥근모서리 이미지셀 xib 생성 (#18)

---
+++ Vegin_iOS/Vegin_iOS/Screen/Feed/Cell/TVC/FeedMainEmptyTVC.swift
@@ -0,0 +1,19 @@
+//
+//  FeedMainEmptyTVC.swift
+//  Vegin_iOS
+//
+//  Created by EUNJU on 2022/02/25.
+//
+
+import UIKit
+
+class FeedMainEmptyTVC: BaseTVC {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
