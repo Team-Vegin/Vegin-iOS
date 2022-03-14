04979251bdab198b95cd46eaa54f67e72c7dd847
eunjuchoi <eunjuzzoo@naver.com>
2022-03-14 14:28:47 +0900

    ➕[ADD] : CategoryCVC 생성 (#31)

---
+++ Vegin_iOS/Vegin_iOS/Screen/Feed/Cell/CVC/CategoryCVC.swift
@@ -0,0 +1,46 @@
+//
+//  CategoryCVC.swift
+//  Vegin_iOS
+//
+//  Created by EUNJU on 2022/03/14.
+//
+
+import UIKit
+
+class CategoryCVC: BaseCVC {
+
+    // MARK: IBOutlet
+    @IBOutlet weak var categoryLabel: UILabel!
+    
+    // MARK: Properties
+    override var isSelected: Bool {
+        didSet {
+            if isSelected {
+                categoryLabel.backgroundColor = .darkMain
+                categoryLabel.textColor = .white
+                categoryLabel.font = .PretendardSB(size: 14)
+            } else {
+                categoryLabel.backgroundColor = .defaultGray
+                categoryLabel.textColor = .defaultTextGray
+                categoryLabel.font = .PretendardR(size: 14)
+            }
+        }
+    }
+    
+    // MARK: Life Cycle
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        configureUI()
+    }
+    
+    // MARK: Custom Methods
+    private func configureUI() {
+        categoryLabel.backgroundColor = .defaultGray
+        categoryLabel.makeRounded(cornerRadius: 14.adjusted)
+        categoryLabel.textColor = .defaultTextGray
+    }
+    
+    func setCategoryData(categoryData: String) {
+        categoryLabel.text = categoryData
+    }
+}
