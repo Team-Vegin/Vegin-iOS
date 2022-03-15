4441d5a40e3742f8d42b6d046955cdf4f5e8c68f
eunjuchoi <eunjuzzoo@naver.com>
2022-03-15 23:51:04 +0900

    ➕[ADD] : FeedNC 파일추가 (#36)

---
+++ Vegin_iOS/Vegin_iOS/Screen/Feed/VC/FeedNC.swift
@@ -0,0 +1,15 @@
+//
+//  FeedNC.swift
+//  Vegin_iOS
+//
+//  Created by EUNJU on 2022/03/15.
+//
+
+import UIKit
+
+class FeedNC: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
