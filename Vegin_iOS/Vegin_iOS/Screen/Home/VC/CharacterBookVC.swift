9378924dfa9c624278f906f0ea16619126187151
Taaeun <tori0403@kakao.com>
2022-03-18 23:05:02 +0900

    💄[MOD] : IBOutlet 연결 (#24)

---
+++ Vegin_iOS/Vegin_iOS/Screen/Home/VC/CharacterBookVC.swift
@@ -0,0 +1,16 @@
+//
+//  CharacterBookVC.swift
+//  Vegin_iOS
+//
+//  Created by Taeeun Lee on 2022/03/16.
+//
+
+import UIKit
+
+
+class CharacterBookVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- Vegin_iOS/Vegin_iOS/Screen/Home/VC/FarmVC.swift
+++ Vegin_iOS/Vegin_iOS/Screen/Home/VC/FarmVC.swift
@@ -3,13 +3,30 @@
 //  Vegin_iOS
 //
 //  Created by EUNJU on 2021/11/24.
+//  Edited by TAEEUN on 2022/03/15.
 //
 
 import UIKit
 
-class FarmVC: UIViewController {
 
+class FarmVC: UIViewController {
+    
+    // MARK: IBOutlet
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var characterBookBtn: UIButton!
+    @IBOutlet weak var mainCharacterBtn: UIButton!
+    @IBOutlet weak var tapProgressBarBtn: UIButton!
+    
+    // MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    // MARK: IBAction
+    @IBAction func tapCharacterPageBtn(_ sender: Any) {  //캐릭터 도감 페이지로 이동
+    }
+    @IBAction func tapCharaterBtn(_ sender: Any) {  //메인 캐릭터를 누르면 새로운 메세지로 변경
+    }
+    @IBAction func tapProgressBarBtn(_ sender: Any) {   //progressBar 누르면 진행중인 미션 노출
     }
 }
