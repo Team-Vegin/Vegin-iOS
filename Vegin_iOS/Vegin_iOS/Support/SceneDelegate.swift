8b3dd87bfc260b215c7cfdc18f9896764a0b7bf9
eunjuchoi <eunjuzzoo@naver.com>
2022-03-26 02:37:26 +0900

    ✅[CHORE] : rootViewController 로그인 뷰로 변경 (#39)

--- Vegin_iOS/Vegin_iOS/Support/SceneDelegate.swift
+++ Vegin_iOS/Vegin_iOS/Support/SceneDelegate.swift
@@ -17,6 +17,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            
+            window.rootViewController = UIStoryboard.init(name: "SignInSB", bundle: nil).instantiateViewController(withIdentifier: SignInVC.className) as? SignInVC
+            self.window = window
+            window.makeKeyAndVisible()
+        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
