668d34b8ff97562a093e1b4348dc2ce296208cff
Taaeun <tori0403@kakao.com>
2022-03-25 13:22:22 +0900

    ➕[ADD] : 도감 뷰 라이브러리 추가 (#24)

---
+++ Vegin_iOS/Vegin_iOS/Screen/Home/Cell/CharacterBookCVC.swift
@@ -0,0 +1,17 @@
+//
+//  CharacterBookCVC.swift
+//  Vegin_iOS
+//
+//  Created by Taeeun Lee on 2022/03/24.
+//
+
+import UIKit
+
+class CharacterBookCVC: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- Vegin_iOS/Vegin_iOS/Screen/Home/VC/CharacterBookVC.swift
+++ Vegin_iOS/Vegin_iOS/Screen/Home/VC/CharacterBookVC.swift
@@ -1,20 +1,12 @@
 //
-//  CharacterBookVC.swift
+//  CharacterBookCV.swift
 //  Vegin_iOS
 //
-//  Created by Taeeun Lee on 2022/03/16.
+//  Created by Taeeun Lee on 2022/03/24.
 //
 
 import UIKit
 
-
-class CharacterBookVC: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
+class CharacterBookCV: UIViewController {
     
-    @IBAction func tapNaviBackBtn(_ sender: UIButton) {
-        self.navigationController?.popViewController(animated: true)
-    }
 }
