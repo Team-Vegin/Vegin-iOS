60345b1c99b221da9d31685a4f74822a81c66cea
eunjuchoi <eunjuzzoo@naver.com>
2022-03-28 23:26:49 +0900

    ✨[FEAT] : 닉네임 설정 뷰 디폴트 UI 구성 (#41)

---
+++ Vegin_iOS/Vegin_iOS/Screen/SignUp/VC/NickNameVC.swift
@@ -0,0 +1,29 @@
+//
+//  NickNameVC.swift
+//  Vegin_iOS
+//
+//  Created by EUNJU on 2022/03/28.
+//
+
+import UIKit
+
+class NickNameVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
