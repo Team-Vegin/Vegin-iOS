628383f6ae32191e2c49b257a9ff52bd410a77e9
eunjuchoi <eunjuzzoo@naver.com>
2022-04-02 09:38:59 +0900

    ➕[ADD] : 알럿 생성 메서드 추가 (#45)

--- Vegin_iOS/Vegin_iOS/Global/Extension/UIViewController+.swift
+++ Vegin_iOS/Vegin_iOS/Global/Extension/UIViewController+.swift
@@ -93,6 +93,19 @@ extension UIViewController {
         self.present(alertViewController, animated: true, completion: completion)
     }
     
+    /// 확인 버튼 Alert 메서드
+    func makeAlert(title : String, message : String? = nil,
+                   okTitle: String = "확인", okAction : ((UIAlertAction) -> Void)? = nil,
+                   completion : (() -> Void)? = nil) {
+        let generator = UIImpactFeedbackGenerator(style: .medium)
+        generator.impactOccurred()
+        let alertViewController = UIAlertController(title: title, message: message,
+                                                    preferredStyle: .alert)
+        let okAction = UIAlertAction(title: okTitle, style: .default, handler: okAction)
+        alertViewController.addAction(okAction)
+        self.present(alertViewController, animated: true, completion: completion)
+    }
+    
     /**
      - Description: 날짜 관련 메서드
      */
