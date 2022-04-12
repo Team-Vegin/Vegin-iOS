3536f79797d2f82d0335b867809940dad89bc512
eunjuchoi <eunjuzzoo@naver.com>
2022-04-13 03:56:17 +0900

    ➕[ADD] : 게시글 상세조회 API 추가 (#62)

--- Vegin_iOS/Vegin_iOS/Network/APIManagers/FeedAPI.swift
+++ Vegin_iOS/Vegin_iOS/Network/APIManagers/FeedAPI.swift
@@ -49,6 +49,22 @@ extension FeedAPI {
             }
         }
     }
+    
+    /// [GET] 게시글 상세 조회
+    func getFeedDetailPostAPI(postID: Int, completion: @escaping (NetworkResult<Any>) -> (Void)) {
+        provider.request(.getFeedDetailPost(postID: postID)) { result in
+            switch result {
+            case .success(let response):
+                let statusCode = response.statusCode
+                let data = response.data
+                
+                completion(self.getFeedDetailPostJudgeData(status: statusCode, data: data))
+                
+            case .failure(let err):
+                print(err.localizedDescription)
+            }
+        }
+    }
 }
 
 // MARK: - judgeData
@@ -76,6 +92,25 @@ extension FeedAPI {
         let decoder = JSONDecoder()
         
         guard let decodedData = try? decoder.decode(GenericResponse<[FeedListDataModel]>.self, from: data) else { return .pathErr }
+        
+        switch status {
+        case 200:
+            return .success(decodedData.data ?? "None-Data")
+        case 400:
+            return .requestErr(decodedData.message)
+        case 404:
+            return .requestErr(404)
+        case 500:
+            return .serverErr
+        default:
+            return .networkFail
+        }
+    }
+    
+    private func getFeedDetailPostJudgeData(status: Int, data: Data) -> NetworkResult<Any> {
+        let decoder = JSONDecoder()
+        
+        guard let decodedData = try? decoder.decode(GenericResponse<FeedPostDataModel>.self, from: data) else { return .pathErr }
         
         switch status {
         case 200:
