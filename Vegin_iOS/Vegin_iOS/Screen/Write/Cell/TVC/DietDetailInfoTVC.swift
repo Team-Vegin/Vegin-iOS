0fef80be4ddf6084222a060943a776f9054b61b3
eunjuchoi <eunjuzzoo@naver.com>
2022-04-17 01:25:15 +0900

    ✅[CHORE] : 서버로부터 받아온 데이터 바인딩 (#68)

--- Vegin_iOS/Vegin_iOS/Screen/Write/Cell/TVC/DietDetailInfoTVC.swift
+++ Vegin_iOS/Vegin_iOS/Screen/Write/Cell/TVC/DietDetailInfoTVC.swift
@@ -9,15 +9,38 @@ import UIKit
 
 class DietDetailInfoTVC: BaseTVC {
 
+    // MARK: IBOutlet
+    @IBOutlet weak var mealTimeLabel: UILabel!
+    @IBOutlet weak var mealAmountLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    }
+    
+    func setData(dietData: DietPostDataModel) {
+        if dietData.mealTime == 1 {
+            mealTimeLabel.text = "아침"
+        } else if dietData.mealTime == 2 {
+            mealTimeLabel.text = "아점"
+        } else if dietData.mealTime == 3 {
+            mealTimeLabel.text = "점심"
+        } else if dietData.mealTime == 4 {
+            mealTimeLabel.text = "점저"
+        } else if dietData.mealTime == 5 {
+            mealTimeLabel.text = "저녁"
+        }
+        
+        if dietData.amount == 1 {
+            mealAmountLabel.text = "조금"
+        } else if dietData.amount == 2 {
+            mealAmountLabel.text = "보통"
+        } else if dietData.amount == 3 {
+            mealAmountLabel.text = "많이"
+        }
     }
     
 }
