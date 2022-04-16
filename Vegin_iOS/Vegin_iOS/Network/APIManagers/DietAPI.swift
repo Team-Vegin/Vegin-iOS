b2da4ea64826d07265cae40c7ef4507604a5f78d
eunjuchoi <eunjuzzoo@naver.com>
2022-04-16 22:42:03 +0900

    ➕[ADD] : 하루 식단 리스트 조회 API 추가 (#66)

--- Vegin_iOS/Vegin_iOS/Network/APIManagers/DietAPI.swift
+++ Vegin_iOS/Vegin_iOS/Network/APIManagers/DietAPI.swift
@@ -10,5 +10,46 @@ import Moya
 
 class DietAPI {
     static let shared = DietAPI()
-    var userProvider = MoyaProvider<DietService>(plugins: [NetworkLoggerPlugin()])
+    var provider = MoyaProvider<DietService>(plugins: [NetworkLoggerPlugin()])
+}
+
+// MARK: - API
+extension DietAPI {
+    /// [GET] 하루 식단 리스트 조회
+    func getDietListAPI(date: String, completion: @escaping (NetworkResult<Any>) -> (Void)) {
+        provider.request(.getDietList(date: date)) { result in
+            switch result {
+            case .success(let response):
+                let statusCode = response.statusCode
+                let data = response.data
+                
+                completion(self.getDietListJudgeData(status: statusCode, data: data))
+                
+            case .failure(let err):
+                print(err.localizedDescription)
+            }
+        }
+    }
+}
+
+// MARK: - judgeData
+extension DietAPI {
+    private func getDietListJudgeData(status: Int, data: Data) -> NetworkResult<Any> {
+        let decoder = JSONDecoder()
+        
+        guard let decodedData = try? decoder.decode(GenericResponse<[DietListDataModel]>.self, from: data) else { return .pathErr }
+        
+        switch status {
+        case 200:
+            return .success(decodedData.data ?? "None-Data")
+        case 400:
+            return .requestErr(decodedData.message)
+        case 404:
+            return .requestErr(404)
+        case 500:
+            return .serverErr
+        default:
+            return .networkFail
+        }
+    }
 }
