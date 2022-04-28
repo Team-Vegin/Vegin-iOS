c632e184794b6093a51bddb0d616134710d0c31d
eunjuchoi <eunjuzzoo@naver.com>
2022-04-29 05:08:34 +0900

    ✨[FEAT] : 미션 시작/중단 UI 구현 (#81)

--- Vegin_iOS/Vegin_iOS/Screen/Home/Cell/CharacterBookCVC.swift
+++ Vegin_iOS/Vegin_iOS/Screen/Home/Cell/CharacterBookCVC.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class CharacterBookCVC: BaseCVC {
+    
+    // MARK: IBOutlet
     @IBOutlet weak var characterImgView: UIImageView!
     @IBOutlet weak var characterNameLabel: UILabel!
     @IBOutlet weak var firstMissionLabel: UILabel!
@@ -20,19 +22,52 @@ class CharacterBookCVC: BaseCVC {
         }
     }
     
+    // Properties
+    var cellTag: Int = -1
+    var isClicked: Bool = false
+    var delegate: SendDataDelegate?
+    
     override func awakeFromNib() {
         super.awakeFromNib()
     }
-
+    
+    /// CVC 재사용 문제 해결을 위한 reload
+    override func prepareForReuse() {
+        chooseBtn.isHidden = false
+    }
+    
+    @IBAction func tapStartBtn(_ sender: UIButton) {
+        if let delegate = delegate {
+            if isClicked {
+                delegate.presentAlert()
+            } else {
+                delegate.sendData(data: cellTag)
+                isClicked = true
+            }
+        }
+    }
 }
 
 // MARK: - Custom Methods
 extension CharacterBookCVC {
-    func setData(characterData: CharacterBookData) {
+    func setData(characterData: CharacterBookData, tagData: Int) {
         characterImgView.image = characterData.makeCharacterImg()
         characterNameLabel.text = characterData.characterName
         firstMissionLabel.text = characterData.firstMission
         secondMissionLabel.text = characterData.secondMission
         thirdMissionLabel.text = characterData.thirdMission
+        cellTag = tagData
+    }
+    
+    func setDefaultBtnUI() {
+        chooseBtn.setImage(UIImage(named: "cheerUp_light"), for: .normal)
+        chooseBtn.backgroundColor = .darkMain
+        chooseBtn.setTitleWithStyle(title: " 도전하기", size: 16, weight: .bold)
+    }
+    
+    func setSelectedBtnUI() {
+        chooseBtn.setImage(UIImage(named: "delete_light"), for: .normal)
+        chooseBtn.backgroundColor = .gray0
+        chooseBtn.setTitleWithStyle(title: " 미션 중단하기", size: 16, weight: .bold)
     }
 }
--- Vegin_iOS/Vegin_iOS/Screen/Home/VC/CharacterBookVC.swift
+++ Vegin_iOS/Vegin_iOS/Screen/Home/VC/CharacterBookVC.swift
@@ -12,6 +12,8 @@ class CharacterBookVC: UIViewController {
     
     // MARK: Properties
     var charcterBookData: [CharacterBookData] = []
+    var selectedIndex: Int = -1
+    var cellTagDelegate: SendDataDelegate?
     
     
     override func viewDidLoad() {
@@ -38,7 +40,7 @@ extension CharacterBookVC {
     private func registerCVC() {
         CharacterBookCVC.register(target: characterBookCV)
     }
-    
+
     private func initData() {
         charcterBookData.append(contentsOf: [
             CharacterBookData(characterImgName: "Tomavi_1", characterName: "토마비", firstMission: "처음 기록하기", secondMission: "두번쨰 기록하기", thirdMission: "세번째 기록하기"),
@@ -53,6 +55,27 @@ extension CharacterBookVC {
             CharacterBookData(characterImgName: "Hidden_Hovi", characterName: "호비", firstMission: "처음 기록하기", secondMission: "두번쨰 기록하기", thirdMission: "세번째 기록하기"),
             
         ])
+    }
+}
+
+// MARK: - SendDataDelegate
+extension CharacterBookVC: SendDataDelegate {
+    func presentAlert() {
+        print("보이나")
+        guard let alert = Bundle.main.loadNibNamed(VeginAlertVC.className, owner: self, options: nil)?.first as? VeginAlertVC else { return }
+        alert.showVeginAlert(vc: self, message: "미션을 중단하시겠습니까?", confirmBtnTitle: "확인", cancelBtnTitle: "취소", iconImg: "delete", type: .withDoubleBtn)
+        alert.confirmBtn.press {
+            self.selectedIndex = -1
+            self.characterBookCV.reloadData()
+        }
+        alert.cancelBtn.press {
+            alert.dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    func sendData(data: Int) {
+        selectedIndex = data
+        self.characterBookCV.reloadData()
     }
 }
 
@@ -112,10 +135,25 @@ extension CharacterBookVC: UICollectionViewDataSource {
         return 10
     }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CharacterBookCVC.className, for: indexPath) as? CharacterBookCVC else {
-            return UICollectionViewCell()
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CharacterBookCVC.className, for: indexPath) as? CharacterBookCVC else { return UICollectionViewCell() }
+        
+        cell.setData(characterData: charcterBookData[indexPath.row], tagData: indexPath.row)
+        cell.delegate = self
+        
+        if selectedIndex != -1 {
+            if selectedIndex != indexPath.row {
+                cell.chooseBtn.isHidden = true
+            } else {
+                cell.setSelectedBtnUI()
+            }
+        } else {
+            cell.isClicked = false
+            cell.setDefaultBtnUI()
         }
-        cell.setData(characterData: charcterBookData[indexPath.row])
+        
+        if indexPath.row == 0 {
+            cell.chooseBtn.isHidden = true
+        }
         return cell
     }
 }
