4cd5982e4a270f067d2f4243a4ebdd012ffdb79f
eunjuchoi <eunjuzzoo@naver.com>
2022-05-03 02:55:00 +0900

    ✨[FEAT] : 셀 페이징 기능 구현 (#87)

--- Vegin_iOS/Vegin_iOS/Screen/Home/VC/CharacterBookVC.swift
+++ Vegin_iOS/Vegin_iOS/Screen/Home/VC/CharacterBookVC.swift
@@ -15,9 +15,14 @@ class CharacterBookVC: BaseVC {
     var selectedIndex: Int = -1
     var cellTagDelegate: SendDataDelegate?
     
+    let cellSize = CGSize(width: UIScreen.main.bounds.width * (276/375), height: UIScreen.main.bounds.width * (276/375) * (616/276))
+    var minItemSpacing: CGFloat = 20
+    let cellCount = 10
+    var previousIndex = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        hideTabbar()
         setUpCV()
         registerCVC()
         initData()
@@ -31,9 +36,11 @@ class CharacterBookVC: BaseVC {
 // MARK: - Custom Methods
 extension CharacterBookVC {
     private func setUpCV() {
+        characterBookCV.contentInsetAdjustmentBehavior = .never
+        characterBookCV.contentInset = UIEdgeInsets(top: 0, left: CGFloat(54), bottom: 0, right: CGFloat(54))
+        characterBookCV.decelerationRate = .fast
         characterBookCV.delegate = self
         characterBookCV.dataSource = self
-        characterBookCV.isPagingEnabled = true
         characterBookCV.backgroundColor = .clear
     }
     
@@ -59,54 +66,29 @@ extension CharacterBookVC {
 }
 
 // MARK: - UICollectionViewDelegateFlowLayout
-extension CharacterBookVC: UICollectionViewDelegateFlowLayout, UIScrollViewDelegate {
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        /// width, heigt 설정
-        return CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
-    }
+extension CharacterBookVC: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 0
+        return minItemSpacing
     }
-    /// 상하, 좌우 설정
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return cellSize
     }
+}
+
+// MARK: - UICollectionViewDelegate
+extension CharacterBookVC: UICollectionViewDelegate {
     func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
-        /// item 사이즈와 item 간격 사이즈를 구해 하나의 item 크기로 설정
-        let layout = self.characterBookCV.collectionViewLayout as! UICollectionViewFlowLayout
-        let cellWidthIncludingSpacing = layout.itemSize.width + layout.minimumLineSpacing
-        
-        /// targetContentOffset을 이용하여 x좌표가 얼마나 이동했는지 확인
-        /// 이동한 x좌표 값과 item의 크기를 비교하여 몇 페이징이 될 것인지 값 설정
+        let cellWidthIncludeSpacing = cellSize.width + minItemSpacing
         var offset = targetContentOffset.pointee
-        /// 멈추는 위치 설정
-        let index = (offset.x + scrollView.contentInset.left) / cellWidthIncludingSpacing
-        var roundedIndex = round(index)
-        var currentIndex: CGFloat = 0   // 현재 페이지의 인덱스
+        let index = (offset.x + scrollView.contentInset.left) / cellWidthIncludeSpacing
+        let roundedIndex: CGFloat = round(index)
         
-        /// scrollView,targetContentOffset 좌표 값으로 스크롤 방향 확인
-        /// index를 반올림해서 사용하면 item의 절반 사이즈만큼 스크롤해야 페이징 됨
-        ///  스크롤로 방향을 체크하여 올림, 내림을 사용하면 더 자연스럽게 페이징 가능
-        if scrollView.contentOffset.x > targetContentOffset.pointee.x { roundedIndex = floor(index) }
-        else if scrollView.contentOffset.x < targetContentOffset.pointee.x {
-            roundedIndex = ceil(index)
-        } else {
-            roundedIndex = round(index)
-        }
-        
-        if currentIndex > roundedIndex {
-            currentIndex -= 1
-            roundedIndex = currentIndex
-        } else if currentIndex < roundedIndex {
-            currentIndex += 1
-            roundedIndex = index
-        }
-        
-        /// 페이징 될 좌표값을 targetContentOffset에 대입
-        offset = CGPoint(x: (roundedIndex * cellWidthIncludingSpacing) - scrollView.contentInset.left, y: -scrollView.contentInset.top)
+        offset = CGPoint(x: roundedIndex * cellWidthIncludeSpacing - scrollView.contentInset.left, y: scrollView.contentInset.top)
         targetContentOffset.pointee = offset
     }
 }
+
 
 // MARK: - UICollectionViewDataSource
 extension CharacterBookVC: UICollectionViewDataSource {
