100bdc1d2f557217f2ea622ebe127d1257bf3ea2
eunjuchoi <eunjuzzoo@naver.com>
2022-05-09 01:27:07 +0900

    ✅[CHORE] : 미션 시작/중단 하기 UI 업데이트 코드 수정 (#95)

--- Vegin_iOS/Vegin_iOS/Screen/Home/Cell/CharacterBookCVC.swift
+++ Vegin_iOS/Vegin_iOS/Screen/Home/Cell/CharacterBookCVC.swift
@@ -39,10 +39,9 @@ class CharacterBookCVC: BaseCVC {
     @IBAction func tapStartBtn(_ sender: UIButton) {
         if let delegate = delegate {
             if isClicked {
-                delegate.presentAlert()
+                delegate.presentAlert(data: cellTag)
             } else {
                 delegate.sendData(data: cellTag)
-                isClicked = true
             }
         }
     }
@@ -50,19 +49,37 @@ class CharacterBookCVC: BaseCVC {
 
 // MARK: - Custom Methods
 extension CharacterBookCVC {
-    func setData(characterData: CharacterBookData, tagData: Int) {
+    func setData(characterData: CharacterBookData, tagData: Int, missionData: MissionListResModel) {
         characterImgView.image = characterData.makeCharacterImg()
-        firstImgView.image = characterData.makeFirstCheckImg()
-        secondImgView.image = characterData.makeSecondCheckImg()
-        thirdImgView.image = characterData.makeThirdCheckImg()
         characterNameLabel.text = characterData.characterName
         firstMissionLabel.text = characterData.firstMission
         secondMissionLabel.text = characterData.secondMission
         thirdMissionLabel.text = characterData.thirdMission
         characterExplainLabel.text = characterData.characterExplanation
         cellTag = tagData
+        
+        // 체크, 잠금 이미지 세팅
+        if missionData.progress[0] == 0 {
+            firstImgView.image = UIImage(named: "lock")
+        } else {
+            firstImgView.image = UIImage(named: "check_img")
+        }
+        
+        if missionData.progress[1] == 0 {
+            secondImgView.image = UIImage(named: "lock")
+        } else {
+            secondImgView.image = UIImage(named: "check_img")
+        }
+        
+        if missionData.progress[2] == 0 {
+            thirdImgView.image = UIImage(named: "lock")
+        } else {
+            thirdImgView.image = UIImage(named: "check_img")
+        }
+           
     }
     
+    /// 미션 중이지 않은 버튼 UI 설정 메서드
     func setDefaultBtnUI() {
         chooseBtn.isActivated = true
         chooseBtn.setImage(UIImage(named: "cheerUp_light"), for: .normal)
@@ -70,6 +87,7 @@ extension CharacterBookCVC {
         chooseBtn.setTitleWithStyle(title: " 도전하기", size: 16, weight: .bold)
     }
     
+    /// 미션 중인 버튼 UI 설정 메서드
     func setSelectedBtnUI() {
         chooseBtn.isActivated = true
         chooseBtn.setImage(UIImage(named: "delete_light"), for: .normal)
