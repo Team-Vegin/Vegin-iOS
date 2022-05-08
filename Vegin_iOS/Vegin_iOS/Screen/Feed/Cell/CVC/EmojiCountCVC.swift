6f4748ff637f464ecab3a981be81b54e1cfe28d9
eunjuchoi <eunjuzzoo@naver.com>
2022-05-08 13:13:17 +0900

    ✅[CHORE] : EmojiCountCVC 자잘한 코드 수정 (#91)

--- Vegin_iOS/Vegin_iOS/Screen/Feed/Cell/CVC/EmojiCountCVC.swift
+++ Vegin_iOS/Vegin_iOS/Screen/Feed/Cell/CVC/EmojiCountCVC.swift
@@ -7,10 +7,12 @@
 
 import UIKit
 
-class EmojiCountCVC: UICollectionViewCell {
+class EmojiCountCVC: BaseCVC {
 
     // MARK: IBOutlet
     @IBOutlet weak var bgView: UIView!
+    @IBOutlet weak var emojiImgView: UIImageView!
+    @IBOutlet weak var emojiCountLabel: UILabel!
     
     // MARK: Life Cycle
     override func awakeFromNib() {
@@ -24,5 +26,13 @@ extension EmojiCountCVC {
     private func configureUI() {
         bgView.layer.borderColor = UIColor(red: 0.761, green: 0.875, blue: 0.647, alpha: 1).cgColor
         bgView.layer.borderWidth = 1
+        bgView.makeRounded(cornerRadius: 0.5 * bgView.bounds.size.height)
+    }
+}
+
+extension EmojiCountCVC {
+    // TODO: 서버 연결 후 모델에서 데이터 받아서 넣어주기
+    func setData() {
+        
     }
 }
