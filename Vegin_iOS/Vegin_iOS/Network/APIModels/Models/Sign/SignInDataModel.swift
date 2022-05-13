a0cc400ab7ccca9542da0ba73ccb83d1a1a78bb5
eunjuchoi <eunjuzzoo@naver.com>
2022-05-14 01:40:25 +0900

    🔨[FIX] : 로그인 모델 수정 (#104)

--- Vegin_iOS/Vegin_iOS/Network/APIModels/Models/Sign/SignInDataModel.swift
+++ Vegin_iOS/Vegin_iOS/Network/APIModels/Models/Sign/SignInDataModel.swift
@@ -17,11 +17,12 @@ struct SignInDataModel: Codable {
 struct User: Codable {
     let userID: Int
     let email: String
+    let nickname: String
     let character: Int
     let orientation: String
 
     enum CodingKeys: String, CodingKey {
         case userID = "userId"
-        case email, character, orientation
+        case email, character, nickname, orientation
     }
 }
